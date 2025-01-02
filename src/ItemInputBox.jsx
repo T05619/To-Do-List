91d33b8572e6308f9835400f879ce6fae053250b
T05619 <lukerjodin@gmail.com>
2025-01-02 22:38:01 +0000

    increase input size on phones

--- src/ItemInputBox.jsx
+++ src/ItemInputBox.jsx
@@ -12,7 +12,7 @@ function ItemInputBox(props) {
     }
     
     return (
-        <input type="text" placeholder="Type" onChange={handleChange} onKeyDown={checkIfEnterKey} value={props.Input} className="duration-500 border-2 border-black dark:border-zinc-200 dark:bg-zinc-900 dark:text-white rounded mx-2 my-2 px-1 pb-1 pl-1.5 hover:border-zinc-500 hover:dark:border-zinc-400 w-32 sm:w-64 focus:border-gray-300 outline-none"/>
+        <input type="text" placeholder="Type" onChange={handleChange} onKeyDown={checkIfEnterKey} value={props.Input} className="duration-500 border-2 border-black dark:border-zinc-200 dark:bg-zinc-900 dark:text-white rounded mx-2 my-2 px-1 pb-1 pl-1.5 hover:border-zinc-500 hover:dark:border-zinc-400 w-48 sm:w-64 focus:border-gray-300 outline-none"/>
     );
 }
 
